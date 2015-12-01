5877d4feae26df0059b8d3b15c80ead174cd4a16
michaeltoledovillegas <michaeltoledovillegas@192.168.1.42>
2015-12-01 17:06:08 -0300

    inicio 5:06 pm

---
+++ src/futbol/Conexion.java
@@ -0,0 +1,19 @@
+
+package futbol;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class Conexion {
+    private Connection con;
+    private Statement stmt;
+    private ResultSet rs;
+    String user="root";
+    String pass="123456";
+    
+    
+     
+    
+}
